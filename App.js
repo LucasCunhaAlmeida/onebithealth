8c77451507bc7bcfcf40d9af03758492ad4202a5
LucasCunhaAlmeida <lucascunhaalmeida2003@gmail.com>
2025-06-21 14:58:30 -0300

    Criando o aplicativo onebithealth

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Title from './src/components/title/'; 
+import Main from './src/components/Main/';
+import Form from './src/components/Form/';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Title></Title>
+      <Main></Main>
     </View>
   );
 }
